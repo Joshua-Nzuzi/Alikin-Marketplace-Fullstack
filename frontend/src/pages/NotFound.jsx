7ac633c0af743f40bc2b0b50f6e57c89484504c5
Joshua Nzuzi <nzuzijoshua@gmail.com>
2025-08-20 00:43:28 +0100

    fix alias

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from 'components/ui/Button';
+import Button from "../components/ui/Button";
+
 import Icon from 'components/AppIcon';
 
 const NotFound = () => {
